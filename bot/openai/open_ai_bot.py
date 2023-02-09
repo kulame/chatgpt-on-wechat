9e80f5eb43bf0fbd5569b9feda0372781b370377
kula <kula@live.com>
2023-02-09 12:56:42 +0800

    feat: record time

--- bot/openai/open_ai_bot.py
+++ bot/openai/open_ai_bot.py
@@ -9,6 +9,8 @@ import time
 user_session = dict()
 
 # OpenAI对话模型API (可用)
+
+
 class OpenAIBot(Bot):
     def __init__(self):
         openai.api_key = conf().get('open_ai_api_key')
@@ -27,7 +29,8 @@ class OpenAIBot(Bot):
             logger.debug("[OPEN_AI] session query={}".format(new_query))
 
             reply_content = self.reply_text(new_query, from_user_id, 0)
-            logger.debug("[OPEN_AI] new_query={}, user={}, reply_cont={}".format(new_query, from_user_id, reply_content))
+            logger.debug("[OPEN_AI] new_query={}, user={}, reply_cont={}".format(
+                new_query, from_user_id, reply_content))
             if reply_content and query:
                 Session.save_session(query, reply_content, from_user_id)
             return reply_content
@@ -37,6 +40,7 @@ class OpenAIBot(Bot):
 
     def reply_text(self, query, user_id, retry_count=0):
         try:
+            start = time.time()
             response = openai.Completion.create(
                 model="text-davinci-003",  # 对话模型的名称
                 prompt=query,
@@ -47,15 +51,18 @@ class OpenAIBot(Bot):
                 presence_penalty=0.0,  # [-2,2]之间，该值越大则更倾向于产生不同的内容
                 stop=["#"]
             )
-            res_content = response.choices[0]["text"].strip().rstrip("<|im_end|>")
-            logger.info("[OPEN_AI] reply={}".format(res_content))
+            res_content = response.choices[0]["text"].strip().rstrip(
+                "<|im_end|>")
+            logger.info("[OPEN_AI] reply={} time={}".format(
+                res_content,  time.time() - start))
             return res_content
         except openai.error.RateLimitError as e:
             # rate limit exception
             logger.warn(e)
             if retry_count < 1:
                 time.sleep(5)
-                logger.warn("[OPEN_AI] RateLimit exceed, 第{}次重试".format(retry_count+1))
+                logger.warn(
+                    "[OPEN_AI] RateLimit exceed, 第{}次重试".format(retry_count+1))
                 return self.reply_text(query, user_id, retry_count+1)
             else:
                 return "提问太快啦，请休息一下再问我吧"
@@ -65,14 +72,13 @@ class OpenAIBot(Bot):
             Session.clear_session(user_id)
             return "请再问我一次吧"
 
-
     def create_img(self, query, retry_count=0):
         try:
             logger.info("[OPEN_AI] image_query={}".format(query))
             response = openai.Image.create(
-                prompt=query,    #图片描述
-                n=1,             #每次生成图片的数量
-                size="256x256"   #图片大小,可选有 256x256, 512x512, 1024x1024
+                prompt=query,  # 图片描述
+                n=1,  # 每次生成图片的数量
+                size="256x256"  # 图片大小,可选有 256x256, 512x512, 1024x1024
             )
             image_url = response['data'][0]['url']
             logger.info("[OPEN_AI] image_url={}".format(image_url))
@@ -81,7 +87,8 @@ class OpenAIBot(Bot):
             logger.warn(e)
             if retry_count < 1:
                 time.sleep(5)
-                logger.warn("[OPEN_AI] ImgCreate RateLimit exceed, 第{}次重试".format(retry_count+1))
+                logger.warn(
+                    "[OPEN_AI] ImgCreate RateLimit exceed, 第{}次重试".format(retry_count+1))
                 return self.reply_text(query, retry_count+1)
             else:
                 return "提问太快啦，请休息一下再问我吧"
@@ -108,7 +115,9 @@ class Session(object):
         session = user_session.get(user_id, None)
         if session:
             for conversation in session:
-                prompt += "Q: " + conversation["question"] + "\n\n\nA: " + conversation["answer"] + "<|im_end|>\n"
+                prompt += "Q: " + \
+                    conversation["question"] + "\n\n\nA: " + \
+                    conversation["answer"] + "<|im_end|>\n"
             prompt += "Q: " + query + "\nA: "
             return prompt
         else:
@@ -138,7 +147,6 @@ class Session(object):
         # discard exceed limit conversation
         Session.discard_exceed_conversation(user_session[user_id], max_tokens)
 
-
     @staticmethod
     def discard_exceed_conversation(session, max_tokens):
         count = 0
@@ -146,7 +154,8 @@ class Session(object):
         for i in range(len(session)-1, -1, -1):
             # count tokens of conversation list
             history_conv = session[i]
-            count += len(history_conv["question"]) + len(history_conv["answer"])
+            count += len(history_conv["question"]) + \
+                len(history_conv["answer"])
             count_list.append(count)
 
         for c in count_list:
